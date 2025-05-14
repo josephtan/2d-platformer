18b367bf7b00a36bb9a25cd54db361fd7288d3d0
Thye Jern Tan <josephtan83@Thyes-MBP.modem>
2025-05-15 00:00:26 +1000

    enemy movement and player animation

---
+++ Scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+@export var acceleration : float = 50
+@export var breaking : float = 20
+@export var gravity : float = 400
+@export var jump_force : float = 200
+
+var move_input :float
+
+@onready var sprite : Sprite2D = $Sprite
+@onready var anim : AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta):
+    if not is_on_floor():
+        velocity.y += gravity * delta
+
+
+    #move left and right
+    move_input = Input.get_axis("move_left", "move_right")
+
+    ##movement
+    if move_input != 0:
+        velocity.x = lerp(velocity.x, move_input * move_speed, acceleration * delta)
+    else:
+        velocity.x = lerp(velocity.x, 0.0, breaking * delta)
+
+    #jump
+
+    if Input.is_action_pressed("jump") and is_on_floor():
+        velocity.y = -jump_force
+    else:
+        velocity.y += gravity * delta
+
+    #apply acceleration and breaking
+    move_and_slide()
+
+func _process(_delta):
+    if velocity.x != 0:
+        sprite.flip_h = velocity.x > 0
+        _manage_animation()
+
+
+func _manage_animation():
+    if not is_on_floor():
+        anim.play("jump")
+    elif move_input != 0:
+        anim.play("move")
+    else:
+        anim.play("idle")
